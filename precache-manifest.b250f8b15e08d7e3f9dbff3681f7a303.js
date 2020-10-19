3f3445f9156b82a07efb24c25e85b16a217a9ef1
Sarel <sarhel.s@gmail.com>
2020-10-19 20:38:56 +0300

    Updates

---
+++ precache-manifest.b250f8b15e08d7e3f9dbff3681f7a303.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d428907c01efc0049ccf29e4f05ae49b",
+    "url": "/Sarel-Varoner-13-10-2020/index.html"
+  },
+  {
+    "revision": "d93dd6109b9706dbe935",
+    "url": "/Sarel-Varoner-13-10-2020/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1f8e16468d643364685b",
+    "url": "/Sarel-Varoner-13-10-2020/static/js/2.12a6d874.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "/Sarel-Varoner-13-10-2020/static/js/2.12a6d874.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d93dd6109b9706dbe935",
+    "url": "/Sarel-Varoner-13-10-2020/static/js/main.f16ae6f3.chunk.js"
+  },
+  {
+    "revision": "b9e4076b76c6d1834f62",
+    "url": "/Sarel-Varoner-13-10-2020/static/js/runtime-main.ef8702ba.js"
+  }
+]);
